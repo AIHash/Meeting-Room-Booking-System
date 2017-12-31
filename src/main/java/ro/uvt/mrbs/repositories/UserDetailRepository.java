f479c25630a97e322257b8851d459f5f6baf36e2
ZbRoby <robi1796@yahoo.com>
2017-12-31 15:38:53 +0200

    added UserDetailRepository

---
+++ src/main/java/ro/uvt/mrbs/repositories/UserDetailRepository.java
@@ -0,0 +1,7 @@
+package ro.uvt.mrbs.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.uvt.mrbs.entities.UserDetail;
+
+public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
+}
