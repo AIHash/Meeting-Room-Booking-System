acc05594a9d588da26ddccf64b16d480913d75c3
ZbRoby <robi1796@yahoo.com>
2017-12-31 15:15:55 +0200

    extracted the database properties from the application.yaml into the applicationDB.properties (which is git ignored)

--- src/main/java/ro/uvt/mrbs/MeetingRoomBookingSystemApplication.java
+++ src/main/java/ro/uvt/mrbs/MeetingRoomBookingSystemApplication.java
@@ -2,11 +2,16 @@ package ro.uvt.mrbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ro.uvt.mrbs")
+@PropertySource(value = "classpath:application.yaml")
+@PropertySource(value = "classpath:applicationDB.properties")
 public class MeetingRoomBookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeetingRoomBookingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingRoomBookingSystemApplication.class, args);
+    }
 }
