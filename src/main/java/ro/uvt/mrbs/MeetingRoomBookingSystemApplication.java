ccf499bbb16e8eb87aef2cb79e9e352ce194a336
ZbRoby <robi1796@yahoo.com>
2017-12-31 14:25:46 +0200

    adding the main class and the application.yaml

---
+++ src/main/java/ro/uvt/mrbs/MeetingRoomBookingSystemApplication.java
@@ -0,0 +1,12 @@
+package ro.uvt.mrbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingSystemApplication.class, args);
+	}
+}
