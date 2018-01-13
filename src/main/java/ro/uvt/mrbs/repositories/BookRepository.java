5c2856e4718043e3f9f96dc604e822f9055e656c
ZbRoby <robi1796@yahoo.com>
2018-01-13 12:51:51 +0200

    added new methods to BookRepository

--- src/main/java/ro/uvt/mrbs/repositories/BookRepository.java
+++ src/main/java/ro/uvt/mrbs/repositories/BookRepository.java
@@ -4,8 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ro.uvt.mrbs.entities.Book;
 
 import java.util.Date;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import ro.uvt.mrbs.entities.Room;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByStartDate(Date date);
+
+    List<Book> findAllByRoom(Room room);
+    
+    Page<Book> findAllByRoom(Room room, Pageable pageable);
+    
+    Long countAllByRoom(Room room);
 }
