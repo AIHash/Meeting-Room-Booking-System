1dd42117ad3692ced9a737756effe8143437290c
ZbRoby <robi1796@yahoo.com>
2018-01-13 20:23:28 +0200

    Added new methods to Book and UserDetail repositories

--- src/main/java/ro/uvt/mrbs/repositories/BookRepository.java
+++ src/main/java/ro/uvt/mrbs/repositories/BookRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import ro.uvt.mrbs.entities.Room;
+import ro.uvt.mrbs.entities.User;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
@@ -20,4 +21,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findAllByRoom(Room room, Pageable pageable);
 
     Long countAllByRoom(Room room);
+
+    List<Book> findLast10ByUser(User user);
 }
--- src/main/java/ro/uvt/mrbs/repositories/UserDetailRepository.java
+++ src/main/java/ro/uvt/mrbs/repositories/UserDetailRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ro.uvt.mrbs.entities.UserDetail;
 
 public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
+
+    UserDetail findByUserUsername(String username);
 }
